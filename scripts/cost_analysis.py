9e675c4f568ee75006f4522006249718d535d96f
Patrik Karlström <jp.karlstrom@gmail.com>
2018-05-28 02:37:15 +0200

    Update cost analysis and corresponding data

--- scripts/cost_analysis.py
+++ scripts/cost_analysis.py
@@ -52,10 +52,11 @@ for dirs in all_strategies:
         sizes.append( program_cost["size"] )
         labels.append( program_label )
 
-fig, speed_bp = plt.subplots(1,1)
-fig.suptitle("Distributions of the geometric mean between the estimated costs of each function in program versions")
 
 # speed
+fig, speed_bp = plt.subplots(1,1)
+#fig.suptitle("Distributions of the geometric mean between the estimated speed of each function in program versions")
+
 speed_bp.boxplot(speeds, whis="range")
 speed_bp.set_ylabel("Geometric mean of estimated speed (in cycles)")
 speed_xtickNames = plt.setp(speed_bp, xticklabels=labels)
@@ -63,14 +64,19 @@ plt.setp(speed_xtickNames, rotation=45, fontsize=8)
 speed_bp.axhline(y=llvm_cost["speed"], color="r", linestyle=":", label="Regular LLVM solution")
 speed_bp.legend()
 
+fig.tight_layout()
+plt.savefig(os.path.join(output_dir, "cost_speed.png"))
+
 # size
-#size_bp.boxplot(sizes, whis="range")
-#size_bp.set_ylabel("Geometric mean of estimated size (in ?)")
-#size_xtickNames = plt.setp(size_bp, xticklabels=labels)
-#plt.setp(size_xtickNames, rotation=45, fontsize=8)
-#size_bp.axhline(y=llvm_cost["size"], color="r", linestyle=":", label="Regular LLVM solution")
-#size_bp.legend()
+fig, size_bp = plt.subplots(1,1)
+#fig.suptitle("Distributions of the geometric mean between the estimated size of each function in program versions")
+
+size_bp.boxplot(sizes, whis="range")
+size_bp.set_ylabel("Geometric mean of estimated size (in ?)")
+size_xtickNames = plt.setp(size_bp, xticklabels=labels)
+plt.setp(size_xtickNames, rotation=45, fontsize=8)
+size_bp.axhline(y=llvm_cost["size"], color="r", linestyle=":", label="Regular LLVM solution")
+size_bp.legend()
 
 fig.tight_layout()
-
-plt.savefig(os.path.join(output_dir, "cost.png"))
+plt.savefig(os.path.join(output_dir, "cost_size.png"))
